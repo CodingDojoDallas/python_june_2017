5c71d4e10ee7a02c6350160f80ae59ebf0b057a4
Rib <ribribble@gmail.com>
2017-06-08 18:27:50 -0500

    added portfolio

---
+++ rib/portfolio2/hello.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template  # Import Flask to allow us to create our app.
+app = Flask(__name__)
+    # Global variable __name__ tells Flask whether or not we are running the file
+                         # directly, or importing it as a module.
+@app.route('/')
+def hello_world():
+  return render_template('index.html')
+    # Return the string 'Hello World!' as a response.
+
+@app.route('/success')
+def success():
+	return render_template('success.html')          # The "@" symbol designates a "decorator" which attaches the following
+
+@app.route('/aboutme')
+def aboutme():
+	return render_template('aboutme.html')                         							# function to the '/' route. This means that whenever we send a request to
+                         							# localhost:5000/ we will run the following "hello_world" function.
+app.run(debug=True)      							# Run the app in debug mode.
