d3a2cb16a59d00e2e8caef5a3d7aef8640867c2f
Lizhang_Xie <lizhang.xie@gmail.com>
2017-06-09 18:35:00 -0500

    added Great Number Game and modified the Counter

--- Lizhang_Xie/Counter/server.py
+++ Lizhang_Xie/Counter/server.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template,session,redirect
 app = Flask(__name__)
 app.secret_key = "Welp"
 
-def count():
-    session['count'] = 0
-
 @app.route('/')
 def index():
+    if 'count' not in session:
+        session['count'] = 0
+
     session['count'] += 1
     return render_template('index.html',count=session['count'])
 
